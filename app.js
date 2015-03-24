ba1ad4b8f501f6205c98b9d3bf939e5ec79c3bcf
Wesley Robinson <Robinson.WesleyD@gmail.com>
2015-03-24 18:59:13 -0400

    app.js and routes functional

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var logger = require('morgan');
+var logger = require('morgan')('dev');
 var parser = require('body-parser');
 var swig = require('swig');
 var routes = require('./routes');
@@ -10,11 +10,14 @@ var async = require('async');
 
 var app = express();
 
+//Turn on the server
 app.listen(3000, function(){ console.log("Server is running"); });
 
+//Body parser
 app.use(parser.json());
 app.use(parser.urlencoded({extended: true}));
 
+//SASS middleware connection:
 app.use(sassMiddleware({
 	src: __dirname,
 	dest: path.join(__dirname, 'public'),
@@ -24,12 +27,14 @@ app.use(sassMiddleware({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Making Bower accessible
+app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
+
+//Morgan Logging
 app.use(logger);
 
-app.use('/', routes);
-
-
-
+//Hand off to Routes
+app.get('/', routes);
 
 // catch 404 (i.e., no route was hit) and forward to error handler
 app.use(function(req, res, next) {
