e81669bda698f99063534af6f26ee7c1be800bc4
iandownie <ian.b.downie@gmail.com>
2015-03-25 12:03:11 -0400

    Working on models

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,25 @@ var router = express.Router();
 var models = require('../models');
 var async = require('async');
 
+var Hotel=require('../models').Hotel;
+var Restaurant=require('../models').Restaurant;
+var Place=require('../models').Place;
+var ThingToDo=require('../models').ThingToDo;
+
+
 
 router.get('/', function(req, res, next){
-	res.render('planner');
+	Hotel.find({}, function(err, hotels) {
+	    Restaurant.find({}, function(err, restaurants) {
+	        ThingToDo.find({}, function(err, thingsToDo) {
+	            res.render('planner', {
+	                all_hotels: hotels,
+	                all_restaurants: restaurants,
+	                all_things_to_do: thingsToDo
+            	});
+        	});
+    	});
+	});
 });
 
 
